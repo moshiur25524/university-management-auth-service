11cf1cb3f824c27d4d759da23ded46bec8f64521
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-06-11 18:41:50 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-
-import express, {Application, Request , Response} from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
